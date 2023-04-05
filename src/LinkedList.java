7c34ba5d26675d7b03511a390a4e7e6f70bed5f7
a.dergachev <bestacc@mail.ru>
2023-04-05 14:19:00 +0300

    reversed LinkedList func created

---
+++ src/LinkedList.java
@@ -0,0 +1,40 @@
+public class LinkedList {
+
+    Node head;
+
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    public void printList() {
+        Node current = head;
+
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
+
+
